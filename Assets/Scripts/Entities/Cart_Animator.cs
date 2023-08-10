dac1b0c83d1a3061cbf2752671fbb5f032f6cabe
NotADev9000 <notadev9000@gmail.com>
2023-08-10 12:59:44 +0100

    Animation: re-added randomization to idle anims
    * moved random anim logic to Cart_Animator class
    * removed AnimationEditor util

--- Assets/Scripts/Entities/Cart_Animator.cs
+++ Assets/Scripts/Entities/Cart_Animator.cs
@@ -8,11 +8,27 @@ public class Cart_Animator : MonoBehaviour
 
     private Animator animator;
 
+    private const string ANIMATION_NAME_WAGON_IDLE = "Idle";
+    private const int ANIMATION_LAYER_ID_WAGON = 1;
     private const string ANIMATION_TRIGGER_COW_APPEAR = "Cow_Appear";
+    private const string ANIMATION_BOOL_COW_MIRROR = "Mirror_Idle";
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        SetupCartIdleAnimation();
+        SetupCowIdleAnimation();
+    }
+
+    private void SetupCartIdleAnimation()
+    {
+        animator.Play(ANIMATION_NAME_WAGON_IDLE, ANIMATION_LAYER_ID_WAGON, Random.value);
+    }
+
+    private void SetupCowIdleAnimation()
+    {
+        bool setBool = Random.value > 0.5f;
+        animator.SetBool(ANIMATION_BOOL_COW_MIRROR, setBool);
     }
 
     public void OnLaserHit()
