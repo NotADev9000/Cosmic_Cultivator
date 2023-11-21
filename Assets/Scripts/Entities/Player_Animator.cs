20609ed247057cc58312d98c46321b02dbf89039
NotADev9000 <notadev9000@gmail.com>
2023-11-21 20:11:31 +0000

    Shiny 👀

--- Assets/Scripts/Entities/Player_Animator.cs
+++ Assets/Scripts/Entities/Player_Animator.cs
@@ -5,7 +5,18 @@ using UnityEngine;
 public class Player_Animator : MonoBehaviour
 {
     [Header("Alien")]
-    [SerializeField] private Transform visualAlien;
+    [SerializeField] private Transform alienTransform;
+    [SerializeField] private SpriteRenderer alienSpriteRenderer;
+
+    [Space(5)]
+
+    [SerializeField] private Sprite alienMainSprite;
+    [SerializeField] private Sprite alienRareSprite;
+    [Range(0, 1)]
+    [SerializeField] private float rareSpriteChance;
+
+    [Space(5)]
+
     [Tooltip("How much the Player should rotate when moving left/right")]
     [SerializeField] private float rotateBy = 20f;
     [SerializeField] private float rotateSpeed = 5f;
@@ -15,6 +26,11 @@ public class Player_Animator : MonoBehaviour
     [Header("Laser")]
     [SerializeField] private Transform laserSpawnPosition;
     [SerializeField] private GameObject laserPrefab;
+
+    private void Awake()
+    {
+        alienSpriteRenderer.sprite = UnityEngine.Random.value < rareSpriteChance ? alienRareSprite : alienMainSprite;
+    }
 
     private void Start()
     {
@@ -34,7 +50,7 @@ public class Player_Animator : MonoBehaviour
         Quaternion targetRotation = Quaternion.Euler(0f, 0f, targetAngle);
 
         // Slerp between the current rotation and the target rotation based on rotationSpeed
-        visualAlien.transform.rotation = Quaternion.Slerp(visualAlien.transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+        alienTransform.transform.rotation = Quaternion.Slerp(alienTransform.transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
     }
 
     private void Player_OnLaserShotAction(Collider2D obj)
